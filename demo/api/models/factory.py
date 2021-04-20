8ca110093a0584868389f8fce6049fe7fc7cbe4f
Nayan Jyoti Das <dastms@gmail.com>
2021-04-20 09:43:00 +0530

    Add external_geofence to factory model

--- demo/api/models/factory.py
+++ demo/api/models/factory.py
@@ -6,33 +6,40 @@ from django.contrib.gis.geos import Polygon
 from django.contrib.gis.admin import OSMGeoAdmin
 from uuid import uuid4
 
+
 class Factory(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.TextField()
 
     geofence = gis_models.PolygonField()
-    
+
+    external_geofence = gis_models.PolygonField()
+
     class Meta:
         db_table = "factories"
 
+
 @admin.register(Factory)
 class FactoryAdmin(OSMGeoAdmin):
-    list_display = [
-        "id",
-        "name",
-        "geofence"
-    ]
+    list_display = ["id", "name", "geofence"]
+
 
 class FactorySerializer(serializers.ModelSerializer):
-
     def validate_geofence(self, value):
         print(value)
         if not isinstance(value, Polygon):
             raise serializers.ValidationError("geofence should be a polygon")
 
         return value
-    
+
+    def validate_external_geofence(self, value):
+        print(value)
+        if not isinstance(value, Polygon):
+            raise serializers.ValidationError("geofence should be a polygon")
+
+        return value
+
     class Meta:
         model = Factory
         fields = "__all__"
